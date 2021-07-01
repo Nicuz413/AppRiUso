3e759718fd474143e17a995e7727690d6c87eea4
Nicuz413 <milonicomilo@gmail.com>
2021-07-01 18:43:50 +0200

    1.1.1

---
+++ app/src/main/java/com/example/appriuso/Item.kt
@@ -0,0 +1,7 @@
+package com.example.appriuso
+
+import android.location.Location
+
+class Item(private var userUID: String, private var name: String, private var description: String, private var image: Int, private var location: Location) {
+
+}
--- app/src/main/java/com/example/appriuso/MainActivity.kt
+++ app/src/main/java/com/example/appriuso/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_new -> true //to do
             R.id.action_logout ->{ auth.signOut()
                 val returnToLogin = Intent(this, LoginActivity::class.java)
                 startActivity(returnToLogin)
