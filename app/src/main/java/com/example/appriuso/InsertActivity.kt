067535a6e04e1464b81dbce734175528ad96693b
Nicuz413 <milonicomilo@gmail.com>
2021-07-03 19:44:53 +0200

    1.1.5

--- app/src/main/java/com/example/appriuso/InsertActivity.kt
+++ app/src/main/java/com/example/appriuso/InsertActivity.kt
@@ -1,29 +1,97 @@
 package com.example.appriuso
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.ActivityNotFoundException
-import android.content.DialogInterface
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.location.*
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_insert.*
 
-
-class InsertActivity : AppCompatActivity() {
+class InsertActivity : AppCompatActivity(), OnMapReadyCallback{
+    private var googleMap: GoogleMap? = null
+    private var cameraPosition: CameraPosition? = null
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private val defaultLocation = LatLng(45.829221758297436, 8.821894447020572)
+    private var locationPermissionGranted = false
+
+    private var lastKnownLocation: Location? = null
+    private var previousMarker: Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (savedInstanceState != null) {
+            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION)
+            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION)
+        }
         setContentView(R.layout.activity_insert)
         auth = Firebase.auth
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        searchLocation.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                refreshMap(getLocationFromAddress(this@InsertActivity, query))
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+        })
+    }
+
+    private fun refreshMap(locationFromAddress: LatLng?) {
+        previousMarker?.remove()
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(locationFromAddress ,DEFAULT_ZOOM.toFloat()))
+        previousMarker = googleMap?.addMarker(MarkerOptions()
+            .position(locationFromAddress)
+            .title("Luogo selezionato"))
+    }
+
+    private fun getLocationFromAddress(context: Context, newText: String?) : LatLng? {
+        val coder: Geocoder = Geocoder(context)
+        var selectedLocation: LatLng? = null
+
+        var address: List<Address> = coder.getFromLocationName(newText, 5)
+        if(address == null) return null
+
+        val location: Address = address[0]
+        selectedLocation = LatLng(location.latitude, location.longitude)
+        return selectedLocation
     }
 
     fun takePicture(view: View) {
@@ -40,8 +108,8 @@ class InsertActivity : AppCompatActivity() {
         builder.show()
     }
 
-    val REQUEST_IMAGE_CAPTURE = 1
-    val REQUEST_IMAGE_FROM_GALLERY = 2
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private val REQUEST_IMAGE_FROM_GALLERY = 2
 
     private fun cameraIntent() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
@@ -78,10 +146,120 @@ class InsertActivity : AppCompatActivity() {
                 }
             }
         }
-
     }
 
     fun sendItem(view: View) {
-        //var newItem = Item(auth.uid, editName.text.toString(), description.text.toString(), imageView.id, Location(mapView.x)
+        var newItem = previousMarker?.let { Item(auth.uid, editName.text.toString(), description.text.toString(), imageView.id, it.position)}
+    }
+
+    override fun onMapReady(mapHandle: GoogleMap?) {
+        googleMap = mapHandle
+        getLocationPermission()
+        updateLocationUI()
+        getDeviceLocation()
+    }
+
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        locationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                }
+            }
+        }
+        updateLocationUI()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocationUI() {
+        if (googleMap == null) {
+            return
+        }
+        try {
+            if (locationPermissionGranted) {
+                googleMap?.isMyLocationEnabled = true
+                googleMap?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                googleMap?.isMyLocationEnabled = false
+                googleMap?.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
+                        }
+                    } else {
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        googleMap?.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
+                        googleMap?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        googleMap?.let { googleMap ->
+            outState.putParcelable(KEY_CAMERA_POSITION, googleMap.cameraPosition)
+            outState.putParcelable(KEY_LOCATION, lastKnownLocation)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    companion object {
+        private val TAG = InsertActivity::class.java.simpleName
+        private const val DEFAULT_ZOOM = 15
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+
+        // Keys for storing activity state.
+        private const val KEY_CAMERA_POSITION = "camera_position"
+        private const val KEY_LOCATION = "location"
     }
 }
--- app/src/main/java/com/example/appriuso/Item.kt
+++ app/src/main/java/com/example/appriuso/Item.kt
@@ -1,7 +1,11 @@
 package com.example.appriuso
 
 import android.location.Location
+import com.google.android.gms.maps.model.LatLng
 
-class Item(private var userUID: String?, private var name: String, private var description: String, private var image: Int, private var location: Location) {
+class Item(private var userUID: String?, private var name: String, private var description: String, private var image: Int, private var location: LatLng) {
 
+    override fun toString(): String {
+        return super.toString()
+    }
 }
