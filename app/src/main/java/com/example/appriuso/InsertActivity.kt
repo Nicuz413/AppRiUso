b51f457a32409f928c44823b5db0ae5fa4ad0d4d
Nicuz413 <milonicomilo@gmail.com>
2021-07-03 04:29:10 +0200

    1.1.4

--- app/src/main/java/com/example/appriuso/InsertActivity.kt
+++ app/src/main/java/com/example/appriuso/InsertActivity.kt
@@ -1,9 +1,11 @@
 package com.example.appriuso
 
+import android.app.AlertDialog
 import android.content.ActivityNotFoundException
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
-import android.location.Location
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
@@ -25,12 +27,23 @@ class InsertActivity : AppCompatActivity() {
     }
 
     fun takePicture(view: View) {
-        dispatchTakePictureIntent()
+        val items = arrayOf("Take Photo", "Choose from Library", "Cancel")
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("Add Photo!")
+        builder.setItems(items) { dialog, item ->
+            when(items[item]){
+                "Take Photo" -> cameraIntent()
+                "Choose from Library" -> galleryIntent()
+                "Cancel" -> dialog.dismiss()
+            }
+        }
+        builder.show()
     }
 
     val REQUEST_IMAGE_CAPTURE = 1
+    val REQUEST_IMAGE_FROM_GALLERY = 2
 
-    private fun dispatchTakePictureIntent() {
+    private fun cameraIntent() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         try {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
@@ -39,12 +52,33 @@ class InsertActivity : AppCompatActivity() {
         }
     }
 
+    private fun galleryIntent() {
+        val takePhotoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        try {
+            startActivityForResult(takePhotoIntent, REQUEST_IMAGE_FROM_GALLERY)
+        } catch (e: ActivityNotFoundException) {
+
+        }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            imageView.setImageBitmap(imageBitmap)
+        when(requestCode){
+            REQUEST_IMAGE_CAPTURE -> {
+                if(resultCode == RESULT_OK){
+                    val extras : Bundle? = data?.extras
+                    val bitmap : Bitmap? = extras?.get("data") as Bitmap?
+                    imageView.setImageBitmap(bitmap)
+                }
+            }
+            REQUEST_IMAGE_FROM_GALLERY -> {
+                if(resultCode == RESULT_OK){
+                    val pickedImage : Uri? = data?.data
+                    imageView.setImageURI(pickedImage)
+                }
+            }
         }
+
     }
 
     fun sendItem(view: View) {
